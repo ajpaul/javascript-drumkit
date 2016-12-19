00d1da6ec19f9b24dc04bd104f14549f4963f0e2
Adam <ajpaul89@gmail.com>
2016-12-19 14:44:06 -0500

    Adding the main files

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
 //scripts.js
 
-var drums = document.getElementById("drum").addEventListener("click", function() {
-    console.log("drum click");
+//when the user presses a key
+document.addEventListener('keydown', function(event) {
+
+    var audio = document.querySelector('audio[data-key="' + event.keyCode + '"]');
+    audio.currentTime = 0;
+    audio.play();
+
+    var drumkey = document.querySelector('div[data-key="' + event.keyCode + '"]');
+    drumkey.classList.add('pressed');
 });
-console.log(drums);
+
+//when the "pressed" transition ends
+const drums = Array.from(document.querySelectorAll('.drum'));
+drums.forEach(drum => drum.addEventListener('transitionend', removeTransition));
+
+
+function removeTransition(e) {
+    e.target.classList.remove("pressed");
+}
+
+
+
+
+
